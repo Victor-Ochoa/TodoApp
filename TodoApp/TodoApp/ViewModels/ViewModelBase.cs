698fb39c4df8a839a95f879ad798a7cbad01c3a1
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2020-08-15 23:54:30 -0300

    melhorado o viewModelBase

--- TodoApp/TodoApp/ViewModels/ViewModelBase.cs
+++ TodoApp/TodoApp/ViewModels/ViewModelBase.cs
@@ -1,26 +1,55 @@
-﻿using Prism.Commands;
+﻿using Prism;
+using Prism.AppModel;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TodoApp.ViewModels
 {
-    public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible
+    public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IPageLifecycleAware, IDestructible, IActiveAware
     {
-        protected INavigationService NavigationService { get; private set; }
+        protected INavigationService _navigationService { get; private set; }
+        protected IPageDialogService _pageDialogService { get; private set; }
+        public event EventHandler IsActiveChanged;
 
-        private string _title;
+        private string _title = string.Empty;
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
-        public ViewModelBase(INavigationService navigationService)
+        public bool IsNotBusy => !IsBusy;
+        bool _isBusy;
+        public bool IsBusy
         {
-            NavigationService = navigationService;
+            get => _isBusy;
+            set {
+                if (SetProperty(ref _isBusy, value)) ;
+            }
+        }
+
+        private bool _isActive;
+        public bool IsActive
+        {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value, RaiseIsActiveChanged);
+        }
+
+        public ViewModelBase(INavigationService navigationService, IPageDialogService pageDialogService)
+        {
+            _navigationService = navigationService;
+            _pageDialogService = pageDialogService;
+
+            this.Title = $"Default";
+        }
+        protected virtual void RaiseIsActiveChanged()
+        {
+            IsActiveChanged?.Invoke(this, EventArgs.Empty);
         }
 
         public virtual void Initialize(INavigationParameters parameters)
@@ -39,6 +68,16 @@ namespace TodoApp.ViewModels
         }
 
         public virtual void Destroy()
+        {
+
+        }
+
+        public virtual void OnAppearing()
+        {
+
+        }
+
+        public virtual void OnDisappearing()
         {
 
         }
