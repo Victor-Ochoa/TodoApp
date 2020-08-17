44dd727f3dd52e946e6841fe0719309f9052c592
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2020-08-17 11:06:53 -0300

    use converter para negar o valor no xaml

---
+++ TodoApp/TodoApp/Models/Todo.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    public class Todo : BindableBase
+    {
+        private bool _complete;
+        private string _description;
+
+        public string Description
+        {
+            get => _description;
+            set { SetProperty(ref _description, value); }
+        }
+        public bool Complete
+        {
+            get => _complete;
+            set { SetProperty(ref _complete, value); }
+        }
+    }
+}
--- TodoApp/TodoApp/ViewModels/MainPageViewModel.cs
+++ TodoApp/TodoApp/ViewModels/MainPageViewModel.cs
@@ -4,17 +4,45 @@ using Prism.Navigation;
 using Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using TodoApp.Models;
 
 namespace TodoApp.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public ObservableCollection<Todo> Todos { get; set; }
+
         public MainPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService)
             : base(navigationService, pageDialogService)
         {
             Title = "Main Page";
+
+            Todos = new ObservableCollection<Todo>
+            {
+                new Todo()
+                {
+                    Complete = false,
+                    Description = "todo 01"
+                },
+                new Todo()
+                {
+                    Complete = true,
+                    Description = "todo 02"
+                },
+                new Todo()
+                {
+                    Complete = false,
+                    Description = "todo com nome longo 3"
+                },
+                new Todo()
+                {
+                    Complete = false,
+                    Description = "todo 04"
+                },
+            };
         }
     }
 }
